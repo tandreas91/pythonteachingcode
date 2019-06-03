bc97eadb29d4c3da61f05f12eb02b9b20fa35ccf
Thomas Andreas <tandreas@students.kennesaw.edu>
2019-06-02 21:16:38 -0400

    Update GradeTracker.py
    read

--- 1 gradetracker/GradeTracker.py
+++ 1 gradetracker/GradeTracker.py
@@ -4,10 +4,10 @@ needs to enable several basic functions for teachers
 needs to have login to protect the student data
 '''
 #import libraries first
-import statistics as s
+import statistics as 
 
 #add constants next
-admins = {'Faculty1':'ABC123','Faculty2':'ABC123'}
+admins = {'Thomas':'Andreas','Faculty2':'ABC123'}
 
 #Like the admins above is a dictionary but of students. Dictionaries use curly brackets with colons to associate keys with values. In this case, each student's first name is a key. The values are lists of grades. 
 #Lists are denoted with square brackets. Values are indexed within starting with 0 for the first one. Each value is separated by commas.
